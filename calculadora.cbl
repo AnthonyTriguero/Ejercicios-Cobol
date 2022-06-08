912446e0afe985c7adcfb741d904e6b5f7fbe90c
Saber <35178886+AnthonyTriguero@users.noreply.github.com>
2022-06-08 12:46:20 -0500

    update
    calculadora and cliente

--- calculadora.cbl
+++ calculadora.cbl
@@ -1,48 +1,76 @@
-      ******************************************************************
-      * Author:
-      * Date:
-      * Purpose:
+         ******************************************************************
+      * Author:ANTHONY
+      * Date:6/8/22
+      * Purpose:CALCULADORA
       * Tectonics: cobc
       ******************************************************************
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. YOUR-PROGRAM-NAME.
+       PROGRAM-ID. CALCULADORA.
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
-       01  WS-NUM-1 PIC S9(5)V99.
-       01  WS-NUM-2 PIC S9(5)V9(2).
-       01  WS-RESULT PIC S9(11)V99.
-       01  WS-PRE-ENTRADA PIC 9(1).
-       01  WS-ENTRADA PIC A(7).
-       01  WS-FORMATO PIC -9999999999.99.
+      *VARIABLES CALCULADORA
+      *-----------------------------------
+       01  WS-NUM-1                        PIC S9(5)V99.
+       01  WS-NUM-2                        PIC S9(5)V9(2).
+       01  WS-RESULT                       PIC S9(11)V99.
+       01  WS-PRE-ENTRADA                  PIC 9(1).
+       01  WS-ENTRADA                      PIC 9(1).
+       01  WS-FORMATO                      PIC -99999.99.
+      *-------------------------------------
+      *DATOS DE CLIENTE
+      *-------------------------------------
+        01  WS-CLIENTE.
+           05 WS-DATOS.
+               10 WS-CLIENTE-NAME         PIC A(10).
+               10 WS-CLIENTE-LASTNAME     PIC A(10).
+               10 WS-CLIENTE-CODE         PIC ZZZZZ.ZZ.
+               10 WS-CLIENTE-CELL.
+                   15 WS-CLIENTE-BALANCE  PIC S9(11)V99 COMP-3.
+                   15 WS-CLIENTE-ADDRES   PIC A(10).
+                   15 WS-CLIENTE-STREET-1 PIC X(10).
+                   15 WS-CLIENTE-STREET-2 PIC X(10).
+      *------------------------------------- 
        PROCEDURE DIVISION.
-       MAIN-PROCEDURE.
+       0000-MAIN-PROCEDURE SECTION.
 
-           PERFORM CALCULADORA UNTIL WS-PRE-ENTRADA=2.
-
-
-           STOP RUN.
+           PERFORM 1000-CALCULADORA UNTIL WS-PRE-ENTRADA=3.
+               PERFORM 4001-SALIR.
 
 
-           CALCULADORA.
+           
+
+
+       1000-CALCULADORA SECTION.
+           
                DISPLAY "1.USAR LA CALCULADORA".
-               DISPLAY "2.SALIR".
+               DISPLAY "2.DATOS DEL CLIENTE".
+               DISPLAY "3.SALIR".
+              
                ACCEPT WS-PRE-ENTRADA.
                IF WS-PRE-ENTRADA = 1
-                   PERFORM OBTENER-DATOS
-                   PERFORM PROCEDIMIENTO
+                   PERFORM 2000-OBTENER-DATOS
+                   PERFORM 2000-PROCEDIMIENTO
+                   
                ELSE
-               IF WS-PRE-ENTRADA NOT =2
-                    DISPLAY "VACIO"
+               IF WS-PRE-ENTRADA =2
+                    
+                    PERFORM 3000-DATOS-CLIENTE
 
                ELSE
-                   DISPLAY "ALGO SALIO MAL".
-
-
-           OBTENER-DATOS.
+                  
+               IF WS-PRE-ENTRADA NOT =3
+                    DISPLAY "SALIDA".
+                    
+                   
+       1000-FIN-CALCULADORA SECTION.
+       
+       2000-OBTENER-DATOS SECTION.          
+           
+           
       *    CABEZERA
                DISPLAY "*******************************************"
-               DISPLAY "SELECCIONE UN NUMERO"
+               DISPLAY "SELECCIONE UN NUMERO PARA LA OPERACIÃ“N"
                DISPLAY "1.SUMA"
                DISPLAY "2.RESTA"
                DISPLAY "3.MULTIPLICACION"
@@ -56,34 +84,81 @@
 
                DISPLAY "INDIQUE LA OPERACION"
                ACCEPT WS-ENTRADA.
-
+       1000-FIN-OBTENER-DATOS SECTION.
+       
+       2000-PROCEDIMIENTO SECTION.
 
            PROCEDIMIENTO.
-           IF WS-ENTRADA = "SUMA"
+           IF WS-ENTRADA = 1
+               
                 DISPLAY "HAZ SELECCIONADO SUMA"
                 ADD WS-NUM-1 TO WS-NUM-2 GIVING WS-RESULT
                 MOVE WS-RESULT TO WS-FORMATO
                 DISPLAY "EL RESULTADO ES:  " WS-FORMATO
            ELSE
-           IF WS-ENTRADA = "RESTA"
+           IF WS-ENTRADA = 2
                 DISPLAY "HAZ SELECCIONADO RESTA"
                 SUBTRACT WS-NUM-1 FROM WS-NUM-2 GIVING WS-RESULT
                 MOVE WS-RESULT TO WS-FORMATO
                 DISPLAY "EL RESULTADO ES:  " WS-FORMATO
            ELSE
-           IF WS-ENTRADA = "MULTIPLICACION"
+           IF WS-ENTRADA = 3
                 DISPLAY "HAZ SELECCIONADO MULTIPLICACION"
                 MULTIPLY WS-NUM-1 BY WS-NUM-2 GIVING WS-RESULT
                 MOVE WS-RESULT TO WS-FORMATO
                 DISPLAY "EL RESULTADO ES:  " WS-FORMATO
            ELSE
-           IF WS-ENTRADA = "DIVICION"
+           IF WS-ENTRADA = 4
                 DISPLAY "HAZ SELECCIONADO DIVICION"
                 DIVIDE WS-NUM-1 INTO WS-NUM-2 GIVING WS-RESULT
                 MOVE WS-RESULT TO WS-FORMATO
                 DISPLAY "EL RESULTADO ES:  " WS-FORMATO
            ELSE
-               DISPLAY "¡¡OOPS!! ALGO SALIO MAL ".
+               DISPLAY "Â¡Â¡OOPS!! ALGO SALIO MAL ".
+       2000-FIN-PROCEDIMIENTO SECTION.
+       
+       3000-DATOS-CLIENTE SECTION.
+            
+            MOVE "ANTHONY" TO WS-CLIENTE-NAME.
+            MOVE "TRIGUERO" TO WS-CLIENTE-LASTNAME.
+            MOVE "......." TO WS-CLIENTE-ADDRES.
+            MOVE "VILLA 1" TO WS-CLIENTE-STREET-1.
+            MOVE "VILLA 1" TO WS-CLIENTE-STREET-2.
 
+            
+            
 
-       END PROGRAM YOUR-PROGRAM-NAME.
+            DISPLAY "INGRESE EL CODIGO: ".
+            ACCEPT WS-CLIENTE-CODE
+            DISPLAY "INGRESE EL BALANCE: ".
+            ACCEPT WS-CLIENTE-BALANCE.
+           
+            DISPLAY "*******************************************".
+            DISPLAY "NOMBRE Y APELLEDO DEL CLIENTE: ".
+            DISPLAY WS-CLIENTE-NAME ' ' WS-CLIENTE-LASTNAME.
+            DISPLAY "DIRECCION: " WS-CLIENTE-ADDRES "/" 
+               WS-CLIENTE-STREET-1 "/" WS-CLIENTE-STREET-2.
+            
+            DISPLAY "EL CODIGO ES: " WS-CLIENTE-CODE.
+            DISPLAY "EL BALANCE SELECIONADO ES: " WS-CLIENTE-BALANCE.
+            DISPLAY "*******************************************".
+            
+       
+       3000-FIN-DATOS-CLIENTE SECTION.
+           
+            
+           
+            
+            
+       4000-GENERAL SECTION.
+       4001-SALIR.
+               STOP RUN.
+       
+       4000-FIN-GENERAL SECTION.
+               
+           
+       0000-FIN-MAIN-PROCEDURE SECTION.
+       
+       END PROGRAM CALCULADORA.
+
+       
