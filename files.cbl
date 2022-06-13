d22cb79f3f824efe5db79a769e896543cf17a6ae
Saber <35178886+AnthonyTriguero@users.noreply.github.com>
2022-06-12 21:41:15 -0500

    Create files.cbl

---
+++ files.cbl
@@ -0,0 +1,20 @@
+ENVIRONMENT DIVISION.
+
+INPUT-OUTPUT SECTION.
+FILE-CONTROL.
+* Nombre del archivo.
+SELECT [OPTIONAL].
+* Tipo de dispositivo.
+ASSIGN TO.
+* Tipo de organización.
+ORGANIZATION IS.
+* Modo de acceso al archivo.
+ACCESS MODE IS.
+* Clave del registro.
+RECORD KEY IS.
+* Claves alternativas del registro.
+ALTERNATE RECORD KEY IS.
+* Tipo de dispositivo.
+WITH DUPLICATES.
+* Variable de estado del archivo.
+FILE STATUS IS.
